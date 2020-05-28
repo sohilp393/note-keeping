15c3782fcf869c422f9e657853400932bd6f7627
sohilp393 <sohilp393@gmail.com>
2020-05-28 14:41:06 +0530

    Added notes controller using scaffold

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,66 @@
+# Performs CRUD operation on notes table
+# It was generated with scaffold generator
+class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.json { render :show, status: :created, location: @note }
+      else
+        format.html { render :new }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
+        format.json { render :show, status: :ok, location: @note }
+      else
+        format.html { render :edit }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @note.destroy
+    respond_to do |format|
+      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
+end
