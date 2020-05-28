e9694a8fea99294cecad0d4371002e7a2fca7374
sohilp393 <sohilp393@gmail.com>
2020-05-28 14:44:34 +0530

    Added roles model file

---
+++ app/models/role.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: roles
+#  id                :integer(4)      not null, primary key
+#  note_id           :integer(4)
+#  user_id           :integer(4)
+#  role              :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+class Role < ApplicationRecord
+  belongs_to :note
+  belongs_to :user
+end
