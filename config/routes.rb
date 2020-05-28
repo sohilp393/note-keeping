756af65e70b5e841f24edaf7e5864946cac99d63
sohilp393 <sohilp393@gmail.com>
2020-05-28 14:33:24 +0530

    Changed the route for the home-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users,path: '',path_names:{ sign_in: 'login',sign_out:'logout',sign_up:'register' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register' }
+
+  root to: 'pages#home'
 end
