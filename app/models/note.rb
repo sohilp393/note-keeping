00dbd29c843a297a3a15f35d23edd15183397ec6
sohilp393 <sohilp393@gmail.com>
2020-05-28 14:44:21 +0530

    Added notes model

---
+++ app/models/note.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: notes
+#  id                :integer(4)      not null, primary key
+#  title             :string(255)
+#  body              :text
+#  user_id           :integer(4)
+#  created_at        :datetime
+#  updated_at        :datetime
+class Note < ApplicationRecord
+  belongs_to :user
+  has_many :roles
+  has_many :users, through: :roles, dependent: :delete_all
+
+  validates :title, presence: true, length: { mininmum: 3, maximum: 50 }
+  validates :body, presence: true, length: { mininmum: 10, maximum: 50 }
+end
