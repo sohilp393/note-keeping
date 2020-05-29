bb942cd7db9c95774895a17ffd5008b6bf12f770
sohilp393 <sohilp393@gmail.com>
2020-05-29 16:24:29 +0530

    Added coded to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
+# Application-Controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include DefaultPageContent
 
-  before_action :authenticate_user! 
-
+  before_action :authenticate_user!
 end
