56c970787cf17801b3ccff63a2a865e70158abbe
sohilp393 <sohilp393@gmail.com>
2020-05-30 23:10:36 +0530

    Dashboard controller to display data on dashboard page

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,17 @@
+# Dash-Board Controller
+class DashboardController < ApplicationController
+  def allnotes
+    @allnotes_hash = {}
+    @created_notes = Note.where(user_id: current_user.id)
+    @contributor_notes = Role.where(
+      user_id: current_user.id,
+      role: 'Contributor')
+    @reader_notes = Role.where(user_id: current_user.id, role: 'Reader')
+
+    @allnotes_hash = {
+      'Createdby' => @created_notes,
+      'Contributor' => @contributor_notes,
+      'Reader' => @reader_notes
+    }
+  end
+end
