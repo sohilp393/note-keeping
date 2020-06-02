e6e27f915ac77a9bf0d775aab2ed3588afcc3a18
sohilp393 <sohilp393@gmail.com>
2020-06-02 22:44:57 +0530

    Replaced factory girl with factory_bot

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
-FactoryGirl.define do
+FactoryBot.define do
   factory :note do
     title { Faker::Name.name }
     body  { Faker::Lorem.sentences}
-    user_id { Faker::Number.non_zero_digit }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
-FactoryGirl.define do
+FactoryBot.define do
   factory :user do
-    name	{ Faker::Name.name }
+    name	{ Faker::Name.first_name }
     email	{ Faker::Internet.email }
     password { Faker::Internet.password }
   end
