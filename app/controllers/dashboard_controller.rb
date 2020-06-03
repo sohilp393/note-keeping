3c743114bc9d349eab84b447ee9194142ee26815
sohilp393 <sohilp393@gmail.com>
2020-06-03 14:34:03 +0530

    Refactor the code and write the code in better way.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,4 @@
-# Dash-Board Controller
+# Dash-Board  Controller
 class DashboardController < ApplicationController
   def allnotes
     @created_notes = Note.where(user_id: current_user.id)
@@ -6,11 +6,5 @@ class DashboardController < ApplicationController
       user_id: current_user.id,
       role: 'Contributor')
     @reader_notes = Role.where(user_id: current_user.id, role: 'Reader')
-
-    @allnotes_hash = {
-      'Createdby' => @created_notes,
-      'Contributor' => @contributor_notes,
-      'Reader' => @reader_notes
-    }
   end
 end
