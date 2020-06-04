9517eb8d55d210389db6a07efbfe4922e5a4545f
sohilp393 <sohilp393@gmail.com>
2020-06-04 12:31:30 +0530

    Changed the association to has_many_and_belong_to

--- app/models/note.rb
+++ app/models/note.rb
@@ -8,9 +8,24 @@
 #  created_at        :datetime
 #  updated_at        :datetime
 class Note < ApplicationRecord
-  belongs_to :user
-  has_many :roles
-  has_many :users, through: :roles, dependent: :delete_all
+  # belongs_to :user
+  # has_many :roles
+  # has_many :users, through: :roles, dependent: :delete_all
+  has_and_belongs_to_many :users
 
   validates :title, presence: true, length: { in: 3..50 }
+
+  def self.contributor_roles(user_id)
+    sql = "
+      select * from notes_users
+      where user_id=#{user_id} and role = 'Contributor'"
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def self.reader_roles(user_id)
+    sql = "
+      select * from notes_users
+      where user_id=#{user_id} and role = 'Reader'"
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -7,11 +7,9 @@
 #  role              :string(255)
 #  created_at        :datetime
 #  updated_at        :datetime
-class Role < ApplicationRecord
-  belongs_to :note
-  belongs_to :user
+# class Role < ApplicationRecord
+  # belongs_to :note
+  # belongs_to :user
 
-  validates :user, uniqueness: {
-    scope: :note,
-    message: 'Already assigned to the note' }
-end
+  # validates :user, uniqueness: {scope: :note,message: 'Already assigned to the note' }
+# end
