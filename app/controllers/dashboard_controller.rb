11f82b84a190beca18e7f694e0373aec20e5506b
sohilp393 <sohilp393@gmail.com>
2020-06-04 12:35:02 +0530

    Used class methods of notes to fetch contributor and reader notes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,8 @@
 # Dash-Board  Controller
 class DashboardController < ApplicationController
-  def allnotes
+  def index
     @created_notes = Note.where(user_id: current_user.id)
-    @contributor_notes = Role.where(
-      user_id: current_user.id,
-      role: 'Contributor')
-    @reader_notes = Role.where(user_id: current_user.id, role: 'Reader')
+    @contributor_notes = Note.contributor_roles(current_user.id)
+    @reader_notes = Note.reader_roles(current_user.id)
   end
 end
