218b5c35acf5136a40cb184826f4d27007ecdd53
sohilp393 <sohilp393@gmail.com>
2020-06-05 12:44:52 +0530

    Removed the code to display created notes on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@
 class DashboardController < ApplicationController
   def index
     current_user_id = current_user.id
-    @created_notes = Note.where(user: current_user)
     @contributor_role_notes = User.contributor_role_notes(current_user_id)
     @reader_role_notes = User.reader_role_notes(current_user_id)
   end
